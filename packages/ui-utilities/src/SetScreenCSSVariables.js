036f53be761ee5e17a42760577aed5a348f87e9a
Chris Vince <chrisvince@gmail.com>
2021-04-13 16:21:51 -0400

    Added initial-inner values
    Added --initial-inner-width and --initial-inner-height css variables to
    SetScreenCSSVariables and useSetScreenCSSVariables.

--- packages/ui-utilities/src/SetScreenCSSVariables.js
+++ packages/ui-utilities/src/SetScreenCSSVariables.js
@@ -21,16 +21,20 @@ const SetScreenCSSVariables = () => {
 		return () => window.removeEventListener('resize', handleWindowResize)
 	}, [])
 
+	useEffect(() => {
+		const { innerHeight, innerWidth } = window
+		const { documentElement } = document
+		documentElement.style.setProperty('--initial-inner-height', `${innerHeight}px`)
+		documentElement.style.setProperty('--initial-inner-width', `${innerWidth}px`)
+	}, [])
+
 	const animation = useSpring({ innerHeight, innerWidth })
 
 	return (
 		<animated.style>
-			{to([ animation.innerHeight, animation.innerWidth ], (height, width) => (`
-				:root {
-					--inner-height: ${height};
-					--inner-width: ${width};
-				}
-			`))}
+			{to([ animation.innerHeight, animation.innerWidth ], (height, width) => (
+				`:root {--inner-height: ${height};--inner-width: ${width};}`
+			))}
 		</animated.style>
 	)
 }
--- packages/ui-utilities/src/useSetScreenCSSVariables.js
+++ packages/ui-utilities/src/useSetScreenCSSVariables.js
@@ -2,12 +2,17 @@ import { useEffect } from 'react'
 
 const useSetScreenCSSVariables = () => {
 	useEffect(() => {
+		const { innerHeight, innerWidth } = window
+		const { documentElement } = document
+
+		documentElement.style.setProperty('--initial-inner-height', `${innerHeight}px`)
+		documentElement.style.setProperty('--initial-inner-width', `${innerWidth}px`)
+
 		const handleWindowResize = () => {
-			const { innerHeight, innerWidth } = window
-			const { documentElement } = document
 			documentElement.style.setProperty('--inner-height', `${innerHeight}px`)
 			documentElement.style.setProperty('--inner-width', `${innerWidth}px`)
 		}
+
 		window.addEventListener('resize', handleWindowResize)
 		handleWindowResize()
 		return () => window.removeEventListener('resize', handleWindowResize)
