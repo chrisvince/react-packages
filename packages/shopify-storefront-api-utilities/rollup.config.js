1502a213b15841d91b415388de811a020a83a72c
Chris Vince <chrisvince@gmail.com>
2021-04-22 14:09:58 -0400

    Major update to useShopifyData
    Updated useShopifyData to be more smart. Now you can just pass in
    product or productVariant data to the data prop and it will
    automatically detect what data type the data is and merge it
    accordingly. It detects the data type from the shopify id (which when
    decoded contains the data type).

--- packages/shopify-storefront-api-utilities/rollup.config.js
+++ packages/shopify-storefront-api-utilities/rollup.config.js
@@ -3,6 +3,7 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import livereload from 'rollup-plugin-livereload'
 import resolve from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
+import babel from '@rollup/plugin-babel'
 
 const { ROLLUP_WATCH } = process.env
 
@@ -25,6 +26,11 @@ export default {
 	plugins: [
 		peerDepsExternal(),
 		resolve(),
+		babel({
+			exclude: 'node_modules/**',
+			presets: ['@babel/env', '@babel/preset-react'],
+			babelHelpers: 'bundled',
+		}),
 		commonjs(),
 		!ROLLUP_WATCH && terser(),
 		ROLLUP_WATCH && livereload(),
--- packages/shopify-storefront-api-utilities/src/useShopifyData.js
+++ packages/shopify-storefront-api-utilities/src/useShopifyData.js
@@ -1,64 +1,117 @@
-import { useCallback, useMemo } from 'react'
-import { assoc, dissoc, is, mergeWithKey, omit, pipe, path } from 'ramda'
+import React, { useCallback } from 'react'
+import { assoc, dissoc, is, mergeWithKey, omit, path, pipe, prop } from 'ramda'
 import { decode } from 'shopify-gid'
-import { arrayOf, checkPropTypes, number, object, shape, string } from 'prop-types'
+import { arrayOf, checkPropTypes, func, object, shape } from 'prop-types'
+import formatPrice from './formatPrice'
 import formatPriceRange from './formatPriceRange'
 import selectedOptionsToObject from './selectedOptionsToObject'
 
 const DISPLAY_NAME = 'useShopifyData'
 
-const PRODUCT_DATA_PREFERENCE = {
-	title: 'sanity',
-	shopifyId: 'shopify',
+const DATA_TYPE = {
+	PRODUCT: 'Product',
+	PRODUCT_VARIANT: 'ProductVariant',
 }
 
-const PRODUCT_VARIANT_DATA_PREFERENCE = {
-	shopifyId: 'shopify',
-}
+const PRODUCT_PRESERVE_LEFT = [
+	'shopifyId',
+]
 
-const handleDuplicateVariantDataPoint = (key, shopify, sanity) => {
-	const preference = PRODUCT_VARIANT_DATA_PREFERENCE[key]
-	return preference === 'shopify' ? shopify : sanity
-}
+const PRODUCT_VARIANT_PRESERVE_LEFT = [
+	'shopifyId',
+]
 
-const mergeVariants = (shopifyVariants, sanityVariants) => shopifyVariants.map(shopifyVariant => {
-	const decodedShopifyId = decode(shopifyVariant.shopifyId).id
-	const sanityVariant = sanityVariants.find(x => x.shopifyId.toString() === decodedShopifyId)
-	if (!sanityVariant) {
-		// eslint-disable-next-line no-console
-		console.error(`No sanityVariant found for Shopify ID: ${decodedShopifyId}`)
+const PRODUCT_VARIANT_OMIT_PROPS = [
+	'id',
+	'__typename',
+]
+
+const PRODUCT_OMIT_PROPS = [
+	'id',
+	'__typename',
+]
+
+const handleDuplicateVariantDataPoint = (key, left, right) => {
+	if (key === 'product') {
+		// eslint-disable-next-line no-use-before-define
+		return mergeProduct(left, right)
 	}
-	return mergeWithKey(handleDuplicateVariantDataPoint, shopifyVariant, sanityVariant)
-})
+	const persistLeft = PRODUCT_VARIANT_PRESERVE_LEFT.includes(key)
+	return persistLeft ? left : right
+}
 
-const handleDuplicateProductDataPoint = (key, shopify, sanity) => {
+const mergeVariant = (left, right) => {
+	const mergedVariant = mergeWithKey(
+		handleDuplicateVariantDataPoint,
+		left,
+		right,
+	)
+	const variantWithOmittedProps = omit(PRODUCT_VARIANT_OMIT_PROPS, mergedVariant)
+	return variantWithOmittedProps
+}
+
+const searchForArray = pipe(
+	input => {
+		if (Array.isArray(input)) {
+			return input
+		}
+		if (Array.isArray(input.edges)) {
+			return input.edges
+		}
+		return undefined
+	},
+	input => {
+		if (Array.isArray(input)) {
+			return input.map(item => {
+				if (item.node) {
+					return item.node
+				}
+				return item
+			})
+		}
+		return undefined
+	},
+)
+
+const mergeVariants = (left, right) => {
+	if (!right) {
+		return left
+	}
+	const rightData = searchForArray(right)
+	return left.map(leftItem => {
+		const rightItem = (
+			rightData.find(x => (
+				leftItem.id === x.shopifyId
+				|| x.shopifyId.toString() === decode(leftItem.shopifyId).id
+			))
+		)
+		if (!rightItem) {
+			return leftItem
+		}
+		return mergeVariant(
+			leftItem,
+			rightItem,
+		)
+	})
+}
+
+const handleDuplicateProductDataPoint = (key, left, right) => {
 	if (key === 'variants') {
-		const mergedVariants = mergeVariants(shopify, sanity)
+		const mergedVariants = mergeVariants(left, right)
 		return mergedVariants
 	}
-	const preference = PRODUCT_DATA_PREFERENCE[key]
-	return preference === 'shopify' ? shopify : sanity
+	const persistLeft = PRODUCT_PRESERVE_LEFT.includes(key)
+	return persistLeft ? left : right
 }
 
-const mergeSanityProductsWithShopifyProducts = (shopifyProducts, sanityProducts) => {
-	if (!sanityProducts) {
-		return shopifyProducts
-	}
-
-	return shopifyProducts.map(shopifyProduct => {
-		const decodedShopifyId = decode(shopifyProduct.shopifyId).id
-		const sanityProduct = sanityProducts.find(x => x.shopifyId.toString() === decodedShopifyId)
-		if (!sanityProduct) {
-			// eslint-disable-next-line no-console
-			console.error(`No sanityVariant found for Shopify ID: ${decodedShopifyId}`)
-		}
-		const newMergedProducts = mergeWithKey(
-			handleDuplicateProductDataPoint,
-			shopifyProduct,
-			sanityProduct,
-		)
-		return newMergedProducts
-	})
+const mergeProduct = (left, right) => {
+	const mergedProduct = mergeWithKey(
+		handleDuplicateProductDataPoint,
+		left,
+		right,
+	)
+	const productWithOmittedProps = omit(PRODUCT_OMIT_PROPS, mergedProduct)
+	return productWithOmittedProps
 }
 
 const getVariantsWithNestedProductFromProduct = product => product.variants.map(variant => {
@@ -70,146 +123,215 @@ const renderOptionQueryString = selectedOptions => (
 	selectedOptions.map(({ name, value }) => `${name}=${value}`).join('&')
 )
 
+const manipulateProductVariant = (variantParam, product, options) => {
+	const {
+		renderProductVariantLinkTo,
+		renderProductLinkTo,
+	} = options
+
+	return pipe(
+		variant => {
+			const handle = prop('handle', product) || path([ 'product', 'handle' ], variant)
+			if (!handle || !variant.selectedOptions) {
+				return variant
+			}
+			const linkTo = renderProductVariantLinkTo(handle, variant.selectedOptions)
+			return assoc('linkTo', linkTo, variant)
+		},
+		variant => {
+			if (!variant.shopifyId) {
+				return variant
+			}
+			const decodedShopifyId = decode(variant.shopifyId).id
+			return assoc('decodedShopifyId', decodedShopifyId, variant)
+		},
+		variant => {
+			if (!variant.selectedOptions) {
+				return variant
+			}
+			const selectedOptionsMap = selectedOptionsToObject(variant.selectedOptions)
+			return assoc('selectedOptionsMap', selectedOptionsMap, variant)
+		},
+		variant => {
+			if (!variant.priceV2) {
+				return variant
+			}
+
+			const formattedPrice = formatPrice(variant.priceV2)
+
+			if (!variant.compareAtPriceV2) {
+				return assoc('formattedPrice', <>{formattedPrice}</>, variant)
+			}
+
+			const formattedCompareAtPrice = formatPrice(variant.compareAtPriceV2)
+
+			const renderPrice = (
+				<>
+					<del>{formattedCompareAtPrice}</del>
+					&ensp;
+					<ins>{formattedPrice}</ins>
+				</>
+			)
+			return assoc('formattedPrice', renderPrice, variant)
+		},
+		variant => {
+			if (!variant.product) {
+				return variant
+			}
+			// eslint-disable-next-line no-use-before-define
+			const newProduct = manipulateProduct(variant.product, {
+				renderProductLinkTo,
+				renderProductVariantLinkTo,
+			})
+			return assoc('product', newProduct, variant)
+		},
+	)(variantParam)
+}
+
+const handleProductVariantsManipulation = (variants, product, options) => (
+	variants.map(variant => manipulateProductVariant(variant, product, options))
+)
+
+const manipulateProduct = (productParam, options) => {
+	const {
+		renderProductLinkTo,
+		renderProductVariantLinkTo,
+	} = options
+
+	return pipe(
+		product => {
+			if (!product.handle) {
+				return product
+			}
+			const linkTo = renderProductLinkTo(product.handle)
+			return assoc('linkTo', linkTo, product)
+		},
+		product => {
+			if (!product.priceRange) {
+				return product
+			}
+			const formattedPriceRange = formatPriceRange(product.priceRange)
+			return assoc('formattedPriceRange', formattedPriceRange, product)
+		},
+		product => {
+			if (!product.shopifyId) {
+				return product
+			}
+			const decodedShopifyId = decode(product.shopifyId).id
+			return assoc('decodedShopifyId', decodedShopifyId, product)
+		},
+		product => {
+			if (!product.variants) {
+				return product
+			}
+			const newVariants = handleProductVariantsManipulation(product.variants, product, {
+				renderProductVariantLinkTo,
+			})
+			return assoc('variants', newVariants, product)
+		},
+	)(productParam)
+}
+
+const mergeDataItem = (left, right) => {
+	const { type } = decode(left.shopifyId)
+	switch (type) {
+		case DATA_TYPE.PRODUCT: return mergeProduct(left, right)
+		case DATA_TYPE.PRODUCT_VARIANT: return mergeVariant(left, right)
+		default: return undefined
+	}
+}
+
+const mergeDataHandler = data => (
+	data.reduce((left, right) => (
+		left.map(leftItem => {
+			const rightItem = right.find(item => (
+				leftItem.shopifyId === item.id
+				|| decode(leftItem.shopifyId).id === item.shopifyId.toString()
+			))
+			return mergeDataItem(leftItem, rightItem)
+		})
+	))
+)
+
+const mergeDataSingleHandler = (data, dataSingle) => (
+	dataSingle.reduce((leftItem, rightItem) => {
+		if (!rightItem) {
+			return leftItem
+		}
+		return mergeDataItem(leftItem, rightItem)
+	}, data)
+)
+
+const manipulateDataHandler = (data, options) => data.map(dataItem => {
+	const { type } = decode(dataItem.shopifyId)
+	switch (type) {
+		case DATA_TYPE.PRODUCT: {
+			return manipulateProduct(dataItem, options)
+		}
+		case DATA_TYPE.PRODUCT_VARIANT: {
+			return manipulateProductVariant(dataItem, null, options)
+		}
+		default: return data
+	}
+})
+
 const PROP_TYPES = {
-	allShopifyProduct: shape({
-		nodes: arrayOf(shape({
-			shopifyId: string.isRequired,
-		})).isRequired,
-	}).isRequired,
-	allSanityProduct: shape({
-		nodes: arrayOf(shape({
-			shopifyId: number.isRequired,
-		})).isRequired,
+	data: arrayOf(arrayOf(object)).isRequired,
+	dataSingle: arrayOf(object),
+	filter: func,
+	find: func,
+	findVariant: func,
+	options: shape({
+		renderProductLinkTo: func,
+		renderProductVariantLinkTo: func,
 	}),
-	liveShopifyData: shape({
-		productByHandle: object,
-	}),
+	sort: func,
 }
 
 const useShopifyData = props => {
 	checkPropTypes(PROP_TYPES, props, 'prop', DISPLAY_NAME)
 
 	const {
-		allShopifyProduct,
-		allSanityProduct = {},
-		liveShopifyData = {},
+		data: dataProp,
+		dataSingle,
 		filter,
 		find,
 		findVariant: findVariantProp,
-		sort,
 		options: {
 			renderProductLinkTo = handle => `/shop/${handle}`,
 			renderProductVariantLinkTo = (handle, selectedOptions) => (
 				`/shop/${handle}?${renderOptionQueryString(selectedOptions)}`
 			),
 		} = {},
+		sort,
 	} = props
 
-	const { nodes: shopifyProducts } = allShopifyProduct
-	const { nodes: sanityProducts } = allSanityProduct
-	const liveShopifyProduct = liveShopifyData.productByHandle
+	const mergeData = useCallback(mergeDataHandler, [])
 
-	const mergeSanityProducts = useCallback(products => (
-		mergeSanityProductsWithShopifyProducts(products, sanityProducts)
-	), [ sanityProducts ])
-
-	const manipulateProducts = useCallback(products => products.map(product => {
-		const handleProductVariants = variants => variants.map(pipe(
-			variant => (
-				(product.handle && variant.selectedOptions) ? (
-					assoc(
-						'linkTo',
-						renderProductVariantLinkTo(product.handle, variant.selectedOptions),
-						variant,
-					)
-				) : variant
-			),
-			variant => (
-				variant.shopifyId ? (
-					assoc('decodedShopifyId', decode(variant.shopifyId).id, variant)
-				) : variant
-			),
-			variant => (
-				variant.selectedOptions ? (
-					assoc(
-						'selectedOptionsMap',
-						selectedOptionsToObject(variant.selectedOptions),
-						variant,
-					)
-				) : variant
-			),
-		))
-
-		const handleProducts = pipe(
-			x => (
-				product.handle ? (
-					assoc('linkTo', renderProductLinkTo(product.handle), x)
-				) : x
-			),
-			x => (
-				product.priceRange ? (
-					assoc('formattedPriceRange', formatPriceRange(product.priceRange), x)
-				) : x
-			),
-			x => (
-				product.shopifyId ? (
-					assoc('decodedShopifyId', decode(product.shopifyId).id, x)
-				) : x
-			),
-			x => (
-				product.variants ? (
-					assoc('variants', handleProductVariants(product.variants), x)
-				) : x
-			),
-		)
-		return handleProducts(product)
+	const manipulateData = useCallback(data => manipulateDataHandler(data, {
+		renderProductLinkTo,
+		renderProductVariantLinkTo,
 	}), [ renderProductLinkTo, renderProductVariantLinkTo ])
 
-	const filterProducts = useCallback(products => {
-		if (find) return products.find(find)
-		if (filter) return products.filter(filter)
-		return products
+	const filtering = useCallback(input => {
+		if (find) return input.find(find)
+		if (filter) return input.filter(filter)
+		return input
 	}, [ filter, find ])
 
-	const sortProducts = useCallback(input => {
+	const sorting = useCallback(input => {
 		if (!sort || !is(Array, input)) {
 			return input
 		}
 		return input.sort(sort)
 	}, [ sort ])
 
-	const mergeLiveShopifyProduct = useCallback(input => {
-		if (!liveShopifyProduct || is(Array, input)) {
+	const mergeDataSingle = useCallback(input => {
+		if (!dataSingle || is(Array, input)) {
 			return input
 		}
-		const product = input
-		if (!product.variants) {
-			const strippedLiveProduct = omit([ '__typename' ], liveShopifyProduct)
-			return {
-				...product,
-				...strippedLiveProduct,
-			}
-		}
-
-		const updatedVariants = product.variants.map(sortedVariant => {
-			const liveVariants = path([ 'variants', 'edges' ], liveShopifyProduct)
-			if (!liveVariants) {
-				return sortedVariant
-			}
-			const { node: matchingLiveVariant } = liveVariants.find(({ node }) => (
-				decode(node.id).id === sortedVariant.decodedShopifyId
-			))
-			const strippedLiveVariant = omit([ 'id', '__typename' ], matchingLiveVariant)
-			return { ...sortedVariant, ...strippedLiveVariant }
-		})
-		const strippedLiveProduct = omit([ 'variants', '__typename' ], liveShopifyProduct)
-
-		return {
-			...product,
-			...strippedLiveProduct,
-			variants: updatedVariants,
-		}
-	}, [ liveShopifyProduct ])
+		return mergeDataSingleHandler(input, dataSingle)
+	}, [ dataSingle ])
 
 	const findVariant = useCallback(input => {
 		if (!input || is(Array, input) || !findVariantProp) {
@@ -232,24 +354,23 @@ const useShopifyData = props => {
 		return productWithVariant
 	}, [ findVariantProp ])
 
-	return useMemo(() => (
-		pipe(
-			mergeSanityProducts,
-			manipulateProducts,
-			filterProducts,
-			sortProducts,
-			mergeLiveShopifyProduct,
-			findVariant,
-		)(shopifyProducts)
-	), [
-		filterProducts,
+	const processDataSets = useCallback(data => pipe(
+		mergeData,
+		manipulateData,
+		filtering,
+		sorting,
+		mergeDataSingle,
 		findVariant,
-		mergeLiveShopifyProduct,
-		manipulateProducts,
-		mergeSanityProducts,
-		shopifyProducts,
-		sortProducts,
+	)(data), [
+		filtering,
+		findVariant,
+		manipulateData,
+		mergeData,
+		mergeDataSingle,
+		sorting,
 	])
+
+	return processDataSets(dataProp)
 }
 
 export default useShopifyData
