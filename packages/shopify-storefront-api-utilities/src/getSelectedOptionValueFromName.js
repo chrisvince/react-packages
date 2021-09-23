2cbcf9c7f50151e3761cb3cf0736e23bb83583b2
Chris Vince <chrisvince@gmail.com>
2021-09-23 18:32:58 -0400

    Fixed getSelectedOptionValueFromName issue
    Updated getSelectedOptionValueFromName undefined issue

--- packages/shopify-storefront-api-utilities/src/getSelectedOptionValueFromName.js
+++ packages/shopify-storefront-api-utilities/src/getSelectedOptionValueFromName.js
@@ -1,3 +1,3 @@
 export default (name, selectedOptions) => (
-	selectedOptions.find(selectedOption => selectedOption.name === name).value
+	selectedOptions.find(selectedOption => selectedOption.name === name)?.value
 )
